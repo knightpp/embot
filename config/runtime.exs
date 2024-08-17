46121f4f829f17dccad029895195fbd117220da9
Danylo Kondratiev <knightpp@proton.me>
2024-08-17 09:09:26 +0300

    Change log level in runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,6 +6,16 @@ fetch_env = fn key ->
   System.get_env(key) || dotenv[key] || raise "no configuration provided for #{key}"
 end
 
+with {:ok, var} <- System.fetch_env("LOG_LEVEL") do
+  level =
+    case var do
+      "warning" -> :warning
+      "debug" -> :debug
+    end
+
+  config :logger, level: level
+end
+
 config :embot,
   access_token: fetch_env.("BOT_ACCESS_TOKEN"),
   endpoint: fetch_env.("BOT_ENDPOINT")
