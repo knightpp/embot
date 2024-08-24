4706cdda0973a2ea5b12c7ba4d530846e6e3d781
Danylo Kondratiev <knightpp@proton.me>
2024-08-24 17:30:44 +0300

    Try to create an exception

--- lib/embot/mastodon.ex
+++ lib/embot/mastodon.ex
@@ -9,6 +9,31 @@ defmodule Embot.Mastodon do
     body
   end
 
+  def upload_media(req, data) do
+    with {:ok, %{status: status, body: body}} =
+           Req.post(req, url: "/api/v2/media", form_multipart: data) do
+      case status do
+        200 ->
+          {:ok, body}
+
+        202 ->
+          {:ok, body}
+
+        status ->
+          {
+            :error,
+            # TODO: didn't work
+            # %Embot.Mastodon.Error{
+            %{
+              message: "unexpected status code",
+              body: body,
+              status: status
+            }
+          }
+      end
+    end
+  end
+
   def upload_media!(req, data) do
     case Req.post!(req, url: "/api/v2/media", form_multipart: data) do
       %{status: 200, body: body} -> body
@@ -91,5 +116,18 @@ defmodule Embot.Mastodon do
   def notifications!(req, query_params \\ []) do
     %{status: 200, body: body} = Req.get!(req, url: "/api/v1/notifications", params: query_params)
     body
+  end
+end
+
+defmodule Embot.Mastodon.Error do
+  defexception [:message, :body, :status]
+
+  @impl true
+  def message(%{message: msg, body: body, status: status}) do
+    """
+      Msg: #{msg}
+      Status: #{status}
+      Body: #{body}
+    """
   end
 end
