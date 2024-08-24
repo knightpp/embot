d9e8e01691349ecd0f9d0827c93a1e62bde074e1
Danylo Kondratiev <knightpp@proton.me>
2024-08-24 22:47:49 +0300

    Add test helper module

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,17 @@
+defmodule TestHelper do
+  def start_http_server(plug) do
+    options = [
+      scheme: :http,
+      port: 0,
+      plug: fn conn, _ -> plug.(conn) end,
+      startup_log: false,
+      http_options: [compress: false]
+    ]
+
+    pid = ExUnit.Callbacks.start_supervised!({Bandit, options})
+    {:ok, {_ip, port}} = ThousandIsland.listener_info(pid)
+    %{pid: pid, url: URI.new!("http://127.0.0.1:#{port}")}
+  end
+end
+
 ExUnit.start()
