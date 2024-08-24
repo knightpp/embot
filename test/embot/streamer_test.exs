58a010cfd4f83a777a6a4efe9cc1a01e99caf878
Danylo Kondratiev <knightpp@proton.me>
2024-08-24 17:30:56 +0300

    Wip tests

--- test/embot/streamer_test.exs
+++ test/embot/streamer_test.exs
@@ -3,14 +3,16 @@ defmodule Embot.StreamerTest do
 
   setup do
     Req.Test.set_req_test_to_shared()
+
     Req.Test.stub(__MODULE__, fn conn ->
-      Req.Test.json(conn, [])
+      conn = Plug.Conn.send_chunked(conn, 200)
+      {:ok, conn} = Plug.Conn.chunk(conn, "event: notification\n\n\n")
+      conn
     end)
 
     req = Req.new(plug: {Req.Test, __MODULE__})
 
-    start_link_supervised!({Embot.Streamer, req})
-
+    start_link_supervised!({Embot.Streamer.Producer, req})
     :ok
   end
 
