f9dbbaddbebb5bd488b7188ec6ccbd54128a9554
Danylo Kondratiev <knightpp@proton.me>
2024-08-29 22:36:11 +0300

    Add tests

--- test/embot/fxtwi_test.exs
+++ test/embot/fxtwi_test.exs
@@ -1,6 +1,6 @@
 defmodule Embot.FxtwiTest do
+  use ExUnit.Case, async: true
   alias Embot.Fxtwi
-  use ExUnit.Case
 
   @body File.read!("./test/data/gif.html")
 
@@ -13,5 +13,27 @@ defmodule Embot.FxtwiTest do
              video: "https://gif.fxtwitter.com/tweet_video/GU-mr7Na4AQovSJ.mp4",
              video_mime: "video/mp4"
            }
+  end
+
+  describe "patch_url!" do
+    test "link is x.com" do
+      link = Fxtwi.patch_url!("https://x.com/some/user/id")
+      assert link == "https://fixupx.com/some/user/id"
+    end
+
+    test "link is twitter.com" do
+      link = Fxtwi.patch_url!("https://twitter.com/some/user/id")
+      assert link == "https://fxtwitter.com/some/user/id"
+    end
+
+    test "link is example.com" do
+      execute = fn -> Fxtwi.patch_url!("http://example.com") end
+      assert_raise(RuntimeError, ~r/^unknown/, execute)
+    end
+
+    test "link is empty" do
+      execute = fn -> Fxtwi.patch_url!("") end
+      assert_raise(RuntimeError, ~r/^unknown/, execute)
+    end
   end
 end
